662ac0d1bbe193fb23113ab0aa13f4238107c759
Oluwatosin Oyekanmi <81862571+oluwatosinoyekanmi@users.noreply.github.com>
2025-05-09 10:56:47 +0100

    Update provider.tf

--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -16,31 +16,3 @@
 #   region  = "us-east-1"
 #   profile = "AdministratorAccess-442426875219"
 # }
-
-
-////jenkins
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# AWS Provider using environment variables
-provider "aws" {
-  region  = var.AWS_REGION
-  profile = var.AWS_PROFILE
-}
-
-variable "AWS_REGION" {
-  description = "The AWS region to operate in"
-  type        = string
-}
-
-variable "AWS_PROFILE" {
-  description = "The AWS CLI profile to use"
-  type        = string
-  default     = "AdministratorAccess-442426875219"  # Or leave it empty if no default is set
-}
