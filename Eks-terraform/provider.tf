83c5d0c205c369c5b15bcaa6783b3fcfb7de6fac
oluwatosin <tosinsalami77@gmail.com>
2025-05-09 11:03:41 +0100

    Remove lock

--- Eks-terraform/provider.tf
+++ Eks-terraform/provider.tf
@@ -1,18 +1,18 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
-# # # Configure the AWS Provider
-# # provider "aws" {
-# #   region = "ap-south-1"
-# # }
-
+# # Configure the AWS Provider
 # provider "aws" {
-#   region  = "us-east-1"
-#   profile = "AdministratorAccess-442426875219"
+#   region = "ap-south-1"
 # }
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "AdministratorAccess-442426875219"
+}
